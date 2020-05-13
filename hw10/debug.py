4b54d23b52feb542f5f923412b579be94f2263ee
Anu Raife <anuradharaife2020@u.northwestern.edu>
2020-05-13 18:30:38 -0500

    updated hw10

--- hw10/debug.py
+++ hw10/debug.py
@@ -10,29 +10,29 @@
 ##function that takes in a list of numbers and removes all the negative numbers
 ## [5,4,-1,-6,3,0,-7] => [5,4,3,0]
 def pos_number(x):
-    return x > 0
+    return x>=0
 
-def only_positivees(lis):
-    return map(pos_number,lis)
+def only_positives(lis):
+    return list(filter(pos_number,lis))
 
 
 ##PROBLEM 2
 ##function that takes in a list of strings as input
 # it first asks the user for a word
 # loops through the list printing the strings out
-# but if the string starts with the same letter as the user's, skip it
+# but  if the string starts with the same letter as the user's, skip it
 def first_letter(lis):
     user = input("please put in a word: ")
     for word in lis:
         if word[0] == user[0]:
-            break
+            continue
         else:
-            return word
+            print(word)
 
 ##PROBLEM 3
 ##function that takes in a string as input and changes the second letter to "p"
 def second_letter_p(word):
-    word[1] = "p"
+    word = word.replace((word)[1],"p")
     return word
 
 ##PROBLEM 4
@@ -42,12 +42,11 @@ def second_letter_p(word):
 ##              => {"strawberries":"red", "clementines":"orange", "bananas":"yellow"}
 def lists_to_dict(keys, values):
     Dict = {}
-    for key,value in zip(keys,values):
-        Dict.append(key) = value
+    for k,v in zip(keys,values):
+        Dict[k] = v
     return Dict
 
-
-##PROBLEM 5
+##PROBLEM 5 
 ##function that takes in a list and returns the sum
 ##the different inputs we have to consider are:
 ##  1. empty list, i.e recursive_sum([]) => 0
@@ -56,11 +55,10 @@ def lists_to_dict(keys, values):
 def recursive_sum(nums):
     if nums == []:
         return 0
+    if type(nums[0]) != list:
+        return nums[0] + recursive_sum(nums[1:])
     else:
-        if type(nums[0]) != list:
-            return nums[0] + nums[1:]
-        else:
-            return recursive_sum(nums[0]) + recursive_sum(nums[1:])
+        return recursive_sum(nums[0]) + recursive_sum(nums[1:])
 
 
 ##PROBLEM 6
@@ -68,7 +66,7 @@ def recursive_sum(nums):
 ##we want EVERY string in the file
 def write_list(lis):
     for string in lis:
-        f = open("output.txt", "w")
+        f = open("output.txt", "a")
         f.write(string + "\n")
     f.close()
 
